6a678619ce854dc6c2e6bd60abc2bb4dfddf3d02
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-01-30 14:27:55 -0800

    Added the base node project

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send("<h1>Hello World</h1>")
+// });
+
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
