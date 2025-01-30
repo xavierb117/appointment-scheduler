6fd79fca318f566f97720bcb23c896feb80cf41e
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-01-30 14:47:00 -0800

    Handles form submissions through an array and a post route

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ app.use(express.static('public'));
 
 const PORT = 3000;
 
+const info = [];
+
+app.use(express.urlencoded({extended:true}));
+
 // app.get('/', (req, res) => {
 //     res.send("<h1>Hello World</h1>")
 // });
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+})
+
+app.post('/submit-order', (req, res) => {
+    info.push(req.body);
+    console.log(req.body)
+    res.send(`<h1>Thank you for your appointment ${req.body.fname}</h1>`)
 })
 
 app.listen(PORT, () => {
